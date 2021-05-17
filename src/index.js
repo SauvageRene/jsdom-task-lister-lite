95e366fbf7c741a7a24de6b043290a43672139d8
Rene <renegarcia1864@gmail.com>
2021-05-17 16:10:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+
+// });
+
+const main = document.querySelector("#main-content")
+console.log(main)
+
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  // user input
+  const newTask = document.querySelector("#new-task-description").value
+  // slap it on the document 
+  // where?
+  const taskItem = document.createElement("li");
+  taskItem.innerText = newTask
+
+  taskList.appendChild(taskItem)
+  taskForm.reset()
+
+}) 
